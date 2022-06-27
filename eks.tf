6b03304c04b80a3a8a29372b53e55614a9e7ea46
johndoelblm <101034700+johndoelblm@users.noreply.github.com>
2022-06-27 16:38:20 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -152,7 +152,7 @@ resource "aws_eks_node_group" "eks_nodegroup" {
   }
 
   update_config {
-    max_unavailable = 2
+    max_unavailable = 1
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
