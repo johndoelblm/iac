1696ce4ef01741a23fd8a6eb49dc6cdda48eae47
johndoelblm <101034700+johndoelblm@users.noreply.github.com>
2022-06-27 16:56:21 +0530

    "aws_eks_node_group" "eks_nodegroup"

--- eks.tf
+++ eks.tf
@@ -139,10 +139,43 @@ resource aws_eks_cluster "eks_cluster" {
   }
 }
 
+
+resource "aws_iam_role" "aws_iam_role_nodegroup" {
+  name = "eks-node-group-nodegroup"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.aws_iam_role_nodegroup.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.aws_iam_role_nodegroup.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.aws_iam_role_nodegroup.name
+}
+
+
+
 resource "aws_eks_node_group" "eks_nodegroup" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "eks_nodegroup"
-  node_role_arn   = "${aws_iam_role.iam_for_eks.arn}"
+  node_role_arn   = aws_iam_role.aws_iam_role.arn
   subnet_ids      = aws_subnet.eks_subnet1[*].id
 
   scaling_config {
